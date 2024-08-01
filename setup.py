14371c1a015d093f40aa5d9a9cc457e2d3f5bcbd
Pytholearn <109828387+Pytholearn@users.noreply.github.com>
2024-08-01 22:53:42 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="update_founder",
-    version="0.1.0",
+    name="found_update",
+    version="1.3.5",
     author="Pytholearn-HAZARD(ilia alizade)",
     author_email="police123456789ilia@gmail.com",
     description="A Python package for automatic updating tools.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/Pytholearn/update_founder",
+    url="https://github.com/Pytholearn/found_update",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
