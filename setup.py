5e6a4c9d377d4ea1796d550c29d149632f0b7add
Pytholearn <109828387+Pytholearn@users.noreply.github.com>
2024-08-03 13:35:49 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Auto_Update",
+    name="auto_update_founder",
     version="2.8.12",
     author="Pytholearn-HAZARD(ilia alizade)",
     author_email="police123456789ilia@gmail.com",
     description="A Python package for automatic updating tools.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/Pytholearn/Auto_Update",
+    url="https://github.com/Pytholearn/auto_update_founder",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
